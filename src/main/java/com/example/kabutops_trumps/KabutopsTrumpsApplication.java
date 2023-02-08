67365782dfae5b91dd84ba1bb262eca784925b3d
Wills BNTA Macbook <williamstanistreet0299@gmail.com>
2023-02-08 13:45:45 +0000

    Initial Commit

---
+++ src/main/java/com/example/kabutops_trumps/KabutopsTrumpsApplication.java
@@ -0,0 +1,13 @@
+package com.example.kabutops_trumps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KabutopsTrumpsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KabutopsTrumpsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/kabutops_trumps/KabutopsTrumpsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kabutops_trumps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KabutopsTrumpsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
