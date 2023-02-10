db52115be6d18881a712b29ac5a5c3174c961e94
Bavaharsan Nagarajah <bavaharsan.nagarajah@gmail.com>
2023-02-10 22:29:32 +0000

    added GET request to OwnershipController

--- src/main/java/com/example/kabutops_trumps/controllers/OwnershipController.java
+++ src/main/java/com/example/kabutops_trumps/controllers/OwnershipController.java
@@ -1,4 +1,31 @@
 package com.example.kabutops_trumps.controllers;
 
+import com.example.kabutops_trumps.models.Ownership;
+import com.example.kabutops_trumps.repositories.OwnershipRepository;
+import com.example.kabutops_trumps.services.OwnershipService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/ownerships")
 public class OwnershipController {
+
+    @Autowired
+    OwnershipRepository ownershipRepository;
+
+    @Autowired
+    OwnershipService ownershipService;
+
+    @GetMapping
+    public ResponseEntity<List<Ownership>> getAllOwnerships(){
+        List<Ownership> ownerships = ownershipService.getAllOwnerships();
+        return new ResponseEntity<>(ownerships, HttpStatus.OK);
+    }
+
 }
--- src/main/java/com/example/kabutops_trumps/services/OwnershipService.java
+++ src/main/java/com/example/kabutops_trumps/services/OwnershipService.java
@@ -1,4 +1,19 @@
 package com.example.kabutops_trumps.services;
 
+import com.example.kabutops_trumps.models.Ownership;
+import com.example.kabutops_trumps.repositories.OwnershipRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class OwnershipService {
+
+    @Autowired
+    OwnershipRepository ownershipRepository;
+
+    public List<Ownership> getAllOwnerships() {
+        return ownershipRepository.findAll();
+    }
 }
