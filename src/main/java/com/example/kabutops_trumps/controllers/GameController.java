af9ada8ff835dd4cf6ed2b3b1b87cf2bc2556b06
Bavaharsan Nagarajah <bavaharsan.nagarajah@gmail.com>
2023-02-10 22:39:06 +0000

    created POST request for creating a new game in GameController

--- src/main/java/com/example/kabutops_trumps/controllers/GameController.java
+++ src/main/java/com/example/kabutops_trumps/controllers/GameController.java
@@ -6,9 +6,7 @@ import com.example.kabutops_trumps.services.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -26,6 +24,12 @@ public class GameController {
     public ResponseEntity<List<Game>> getAllGames(){
         List<Game> games = gameService.getAllGames();
         return new ResponseEntity<>(games, HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<Game> addNewReader(@RequestBody Game game) {
+        Game newGame = gameService.startNewGame(game.getPlayerA(), game.getPlayerB());
+        return new ResponseEntity<>(newGame, HttpStatus.CREATED);
     }
 
 }
