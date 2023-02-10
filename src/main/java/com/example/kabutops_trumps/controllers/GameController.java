dacac395e811b5685193cc9337d5503c13dfe7ad
Bavaharsan Nagarajah <bavaharsan.nagarajah@gmail.com>
2023-02-10 23:23:40 +0000

    added PATCH request to GameController to process a round of a game, however it is not working yet

--- src/main/java/com/example/kabutops_trumps/controllers/GameController.java
+++ src/main/java/com/example/kabutops_trumps/controllers/GameController.java
@@ -3,12 +3,14 @@ package com.example.kabutops_trumps.controllers;
 import com.example.kabutops_trumps.models.Game;
 import com.example.kabutops_trumps.repositories.GameRepository;
 import com.example.kabutops_trumps.services.GameService;
+import jakarta.persistence.criteria.CriteriaBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/games")
@@ -30,6 +32,12 @@ public class GameController {
     public ResponseEntity<Game> addNewReader(@RequestBody Game game) {
         Game newGame = gameService.startNewGame(game.getPlayerA(), game.getPlayerB());
         return new ResponseEntity<>(newGame, HttpStatus.CREATED);
+    }
+
+    @PatchMapping(value = "/{id}")
+    public ResponseEntity<Game> updateGame(@PathVariable long id, @RequestParam int statA, @RequestParam int statB){
+        Game updatedGame = gameService.processGame(gameRepository.findById(id).get(), statA, statB);
+        return new ResponseEntity<>(updatedGame, HttpStatus.OK);
     }
 
 }
