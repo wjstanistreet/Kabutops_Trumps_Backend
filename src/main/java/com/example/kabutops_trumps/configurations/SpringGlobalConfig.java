3c08161428ab4a31957bc0abd0c36ccf8261d742
Shawn <shawnaugustine01@gmail.com>
2023-02-10 10:37:33 +0000

    cors file for frontend

---
+++ src/main/java/com/example/kabutops_trumps/configurations/SpringGlobalConfig.java
@@ -0,0 +1,17 @@
+package com.example.kabutops_trumps.configurations;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class SpringGlobalConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry){
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedHeaders("*")
+                .allowedMethods("*");
+    }
+}
