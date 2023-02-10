00f2bb3161e6950d47a1f2fcfe57cf306d03ebd1
Shawn <shawnaugustine01@gmail.com>
2023-02-10 17:26:07 +0000

    added some gamelogic

--- src/main/java/com/example/kabutops_trumps/services/GameService.java
+++ src/main/java/com/example/kabutops_trumps/services/GameService.java
@@ -14,15 +14,58 @@ public class GameService {
     @Autowired
     GameRepository gameRepository;
 
-    public void startNewGame(String accountA, String accountB){
+    public Game startNewGame(String accountA, String accountB){
         Game game = new Game(accountA, accountB);
         gameRepository.save(game);
+        return game;
     }
 
-    public void processRound(int statA, int statB){
-
+    public String processRound(String accountA, String accountB, int statA, int statB){
+        if(statA>statB){
+            return accountA;
+        }
+        if(statB>statA){
+            return accountB;
+        }
+        else{
+            return null;
+        }
     }
 
+    public Game processGame(Game game, int statA, int statB) {
+        int finalRound= 7;
+        Game currentGame = gameRepository.findById(game.getId()).get();
+        int roundNumber = currentGame.getRoundNumber();
+        int scoreA = currentGame.getScoreA();
+        int scoreB = currentGame.getScoreB();
 
+        if(roundNumber<=finalRound) {
+            if (processRound(game.getPlayerA(), game.getPlayerB(), statA, statB).equals(game.getPlayerA())) {
+                scoreA += 1;
+            } else if (processRound(game.getPlayerA(), game.getPlayerB(), statA, statB).equals(game.getPlayerB())) {
+                scoreB += 1;
+            } else {
+                scoreA += 0.5;
+                scoreB += 0.5;
+            }
+            currentGame.setScoreA(scoreA);
+            currentGame.setScoreB(scoreB);
+            roundNumber+=1;
+            currentGame.setRoundNumber(roundNumber);
+            currentGame.setPlayerATurn(!currentGame.isPlayerATurn());
+            if(roundNumber==finalRound){
+                if(scoreA>scoreB){
+                    currentGame.setWinner(game.getPlayerA());
+                }
+                else if(scoreB>scoreA){
+                    currentGame.setWinner(game.getPlayerB());
+                }
+                else{
+                    currentGame.setWinner("Tie");
+                }
+            }
+        }
+        return currentGame;
+    }
 
 }
